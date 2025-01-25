d288b5a74e7babcc018f12e2b69a15c28fdb237b
YOYBROY <ethan.j.grigg@gmail.com>
2025-01-25 21:25:36 +1100

    Added Menus and win and lose conditions

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
+using StarterAssets;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -8,42 +8,96 @@ public class Enemy : MonoBehaviour
     [SerializeField] Transform[] patrolPoints;
     Transform targetPoint;
     int patrolNumber = 0;
-    [SerializeField] float lerpSpeed = 1f;
+    [SerializeField] float patrolLerpSpeed = 2f;
+    [SerializeField] float attackLerpSpeed = 15f;
 
     [SerializeField] float moveSpeed = 1f;
     [SerializeField] float distanceThreshold = 0.01f;
 
+    private GameObject player;
     private GunController gunController;
-    private Material storedMaterial;
-    [SerializeField] private Material stunnedMat;
 
     [SerializeField] float stunThreshold = 500f;
 
+    [SerializeField] float attackRange = 10f;
+    float timer;
+    [SerializeField] float attackTimer = 4f;
+
+    [SerializeField] Transform attackSpawnPoint;
+    [SerializeField] GameObject enemyProjectile;
+
+    float dazedCount;
+    [SerializeField] float dazedTimer = 2f;
+    bool alert;
+
+    enum EnemyState { PATROLLING, ATTACKING, APPROACHING };
+    EnemyState enemyState;
+
     void Start()
     {
+        player = FindObjectOfType<FirstPersonController>().gameObject;
         gunController = FindObjectOfType<GunController>();
-        storedMaterial = GetComponent<Material>();
-        targetPoint = patrolPoints[0]; 
+        targetPoint = patrolPoints[0];
+        timer = attackTimer;
+        PauseMenu.totalEnemyCount++;
     }
 
     private void Update()
     {
-        //STATE 1: PATROLLING
-        //move towards a patrol point.
-        Vector3 direction = targetPoint.position - transform.position;
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        //when we get within a certain range of patrol point.
-        float distance = Vector3.Distance(targetPoint.position, transform.position);
-        if (distance < distanceThreshold) UpdatePatrolPoints();
-        //go to next.
-        //rotate in direction of movement.
-        Quaternion targetRotation = Quaternion.FromToRotation(Vector3.forward, direction.normalized);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, lerpSpeed * Time.deltaTime);
+        if (dazedCount > 0)
+        {
+            dazedCount -= Time.deltaTime;
+            return;
+        }
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer < attackRange) enemyState = EnemyState.ATTACKING;
+        else if (alert) enemyState = EnemyState.APPROACHING;
+        else enemyState = EnemyState.PATROLLING;
 
-        //STATE 2: ATTACKING
-        //face towards player
-        //shoot projectile on a timer
-        //play sound effect
+
+        switch (enemyState)
+        {
+            case EnemyState.PATROLLING:
+                //move towards a patrol point.
+                Vector3 direction = targetPoint.position - transform.position;
+                transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+                //when we get within a certain range of patrol point.
+                float distance = Vector3.Distance(targetPoint.position, transform.position);
+                //go to next point
+                if (distance < distanceThreshold) UpdatePatrolPoints();
+                //rotate in direction of movement.
+                Quaternion targetRotation = Quaternion.FromToRotation(Vector3.forward, direction.normalized);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, patrolLerpSpeed * Time.deltaTime);
+                break;
+            case EnemyState.ATTACKING:
+                alert = true;
+                //face towards player
+                direction = player.transform.position - transform.position;
+                targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, attackLerpSpeed * Time.deltaTime);
+                //shoot projectile on a timer
+                timer -= Time.deltaTime;
+                if (timer < 0)
+                {
+                    //Attack
+                    Debug.Log("Attack");
+                    Instantiate(enemyProjectile, attackSpawnPoint.position, attackSpawnPoint.rotation);
+                    //attack Animation
+                    timer = attackTimer;
+                }
+                //play sound effect
+                break;
+            case EnemyState.APPROACHING:
+                //face towards player
+                direction = player.transform.position - transform.position;
+                targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, attackLerpSpeed * Time.deltaTime);
+                //Mose Towards Player
+                transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+                break;
+            default:
+                break;
+        }
     }
 
     void UpdatePatrolPoints()
@@ -57,17 +111,19 @@ public class Enemy : MonoBehaviour
     {
         if (gunController.canShoot)
         {
+            PauseMenu.totalEnemyCount--;
             Destroy(gameObject);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("PlayerProjectile"))
+        if (collision.gameObject.CompareTag("PlayerProjectile"))
         {
             Debug.Log(collision.relativeVelocity.sqrMagnitude);
             if (collision.relativeVelocity.sqrMagnitude < stunThreshold) return;
-            GetComponent<Renderer>().material = stunnedMat;
+            //Play dazed particle effect
+            dazedCount = dazedTimer;
         }
     }
 }
